e28aba4a768b28dee3265396ee3fe9b68f8df852
Bodea Denis <denis_gabriel94@yahoo.com>
2021-11-14 14:22:35 +0200

    Frontend

--- backend/dist/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var auth_1 = require("../controllers/auth");
-var router = (0, express_1.Router)();
-router.post("/register", auth_1.registerUser);
-router.post("/login", auth_1.loginUser);
-router.post("/token/:id", auth_1.refreshToken);
-exports.default = router;
